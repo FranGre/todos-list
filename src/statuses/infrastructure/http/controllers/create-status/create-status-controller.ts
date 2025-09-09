1aeb9eeeb177ac1e28256a0abe5d058dc90610a5
fran gregori <frangregori61@gmail.com>
2025-09-09 22:35:02 +0200

    infrastructure: improve CreateStatusController responses

--- src/statuses/infrastructure/http/controllers/create-status/create-status-controller.ts
+++ src/statuses/infrastructure/http/controllers/create-status/create-status-controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpException, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
 import { CreateStatusCommand } from 'src/statuses/application/create-status/create-status-command';
 import { CreateStatusUseCase } from 'src/statuses/application/create-status/create-status-use-case';
 import { CreateStatusDto } from './create-status-dto';
@@ -6,6 +6,9 @@ import { CreateStatusResult } from 'src/statuses/application/create-status/creat
 import { StatusNameAlreadyExistsError } from 'src/statuses/domain/errors/status-name-already-exists-error';
 import { StatusNameEmptyError } from 'src/statuses/domain/errors/status-name-empty-error';
 import { StatusNameTooLongError } from 'src/statuses/domain/errors/status-name-too-long-error';
+import { BaseResponse } from 'src/shared/infrastructure/responses/base/base-response';
+import { SuccessResponse } from 'src/shared/infrastructure/responses/success/success-response';
+import { ErrorResponse } from 'src/shared/infrastructure/responses/error/error-response';
 
 @Controller('statuses')
 export class CreateStatusController {
@@ -13,44 +16,25 @@ export class CreateStatusController {
     constructor(private _createStatusUseCase: CreateStatusUseCase) { }
 
     @Post()
-    async execute(@Body() dto: CreateStatusDto) {
+    async execute(@Body() dto: CreateStatusDto): Promise<BaseResponse> {
         const command = new CreateStatusCommand(dto.name);
         try {
             const result: CreateStatusResult = await this._createStatusUseCase.execute(command);
-            return {
-                message: 'Created successfully',
-                status: HttpStatus.CREATED,
-                data: result
-            };
+            return new SuccessResponse(HttpStatus.CREATED, 'status created successfully', result, '/statuses');
         } catch (error) {
             if (error instanceof StatusNameAlreadyExistsError) {
-                throw new HttpException(
-                    {
-                        message: error.message
-                    },
-                    HttpStatus.CONFLICT);
+                return new ErrorResponse(HttpStatus.CONFLICT, error.message, error.name, '/statuses');
             }
 
             if (error instanceof StatusNameEmptyError) {
-                throw new HttpException(
-                    {
-                        message: error.message
-                    },
-                    HttpStatus.CONFLICT);
-            }
-            
-            if (error instanceof StatusNameTooLongError) {
-                throw new HttpException(
-                    {
-                        message: error.message
-                    },
-                    HttpStatus.CONFLICT);
+                return new ErrorResponse(HttpStatus.CONFLICT, error.message, error.name, '/statuses');
             }
 
-            throw new HttpException(
-                { status: 'error', message: 'Internal Server Error' },
-                HttpStatus.INTERNAL_SERVER_ERROR,
-            );
+            if (error instanceof StatusNameTooLongError) {
+                return new ErrorResponse(HttpStatus.CONFLICT, error.message, error.name, '/statuses');
+            }
+
+            return new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, 'Internal Server Error', 'SERVER ERROR', '/statuses');
         }
     }
 }
